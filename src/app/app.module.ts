e8c34b6faf0bcd4547f6c86352098b58171a1c8d
Jeffrey Ng <komoto415@gmail.com>
2020-05-24 15:29:05 -0500

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing.module'; // CLI imports AppRoutingModule
 
 import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ResumeComponent } from './resume/resume.component';
+import { SlidesComponent } from './slides/slides.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    NavbarComponent,
+    ResumeComponent,
+    SlidesComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    AppRoutingModule // CLI adds AppRoutingModule to the AppModule's imports array
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/slides/slides.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slides',
+  templateUrl: './slides.component.html',
+  styleUrls: ['./slides.component.css']
+})
+export class SlidesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
