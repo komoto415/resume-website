a73385c5573558361c7cd2d6aeaef9743364aa76
Jeffrey Ng <jng@stedwards.edu>
2020-05-29 16:01:03 -0500

    temp links to code basis

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -13,6 +13,7 @@ export class ProjectsComponent implements OnInit {
                 thumbnail: "/wego.jpg", // https://ces.eetimes.com/wp-content/uploads/2020/01/image-eet-ces.jpg
                 tags: ["python","html5","javascript", "ngnix", "docker", "python", "swei", "agile-development", "multi-cloud-solution",],
                 desc: "So many teams crashed and burned",
+                link: "https://github.com/sp20-swei-Team22"
             },
         ],
         [
@@ -21,15 +22,16 @@ export class ProjectsComponent implements OnInit {
                 thumbnail: "/kart-test.jpg", // https://www.aoe.com/fileadmin/AOE.com/images/main_navigation/blog/Stock_Photos/miscellaneous/Fotolia_94900081_Chess_Pieces_930_590_70.jpg
                 tags: ["java", "components", "algorithms", "data-structures",],
                 desc: "An open bid for experimental projects that could be assigned in Component-Based Programming and then reduxed for Algorithm and Data Structures",
+                link: "https://github.com/komoto415/TestAssignment",
             },
         ],
         [
-            {
-                title: "To Brock and not to Brock",
-                thumbnail: "/senior-project.png", // https://i.ibb.co/7yMB9t6/FireRed.png
-                tags: ["ml", "pokemon", "python", "julia"],
-                desc: "For my research project, my partner and I decided to teach a machine how to beat Brock in Pokemon Fire Red. If Twitch can do it, why can't a bot?",
-            },
+            // {
+            //     title: "To Brock and not to Brock",
+            //     thumbnail: "/senior-project.png", // https://i.ibb.co/7yMB9t6/FireRed.png
+            //     tags: ["ml", "pokemon", "python", "julia"],
+            //     desc: "For my research project, my partner and I decided to teach a machine how to beat Brock in Pokemon Fire Red. If Twitch can do it, why can't a bot?",
+            // },
         ],
 
     ]
