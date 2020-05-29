e2833e9ccd81ef1851e4bb9b984779097a80e3c5
Jeffrey Ng <jng@stedwards.edu>
2020-05-29 18:09:49 -0500

    ore appropriate flavour text for wego project

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -12,7 +12,7 @@ export class ProjectsComponent implements OnInit {
                 title: "We Go Services",
                 thumbnail: "/wego.jpg", // https://ces.eetimes.com/wp-content/uploads/2020/01/image-eet-ces.jpg
                 tags: ["python","html5","javascript", "ngnix", "docker", "python", "swei", "agile-development", "multi-cloud-solution",],
-                desc: "So many teams crashed and burned",
+                desc: "In the advent of autonomous vehicles, we aim to take advantage of the growing market of Transportation as a Service with a scalable multi-service Web Application to help fulfil everyday needs.",
                 link: "https://github.com/sp20-swei-Team22"
             },
         ],
@@ -21,7 +21,7 @@ export class ProjectsComponent implements OnInit {
                 title: "Components-Algorithms Bridge Assignment",
                 thumbnail: "/kart-test.jpg", // https://www.aoe.com/fileadmin/AOE.com/images/main_navigation/blog/Stock_Photos/miscellaneous/Fotolia_94900081_Chess_Pieces_930_590_70.jpg
                 tags: ["java", "components", "algorithms", "data-structures",],
-                desc: "An open bid for experimental projects that could be assigned in Component-Based Programming and then reduxed for Algorithm and Data Structures",
+                desc: "An open bid for experimental projects that could be assigned in Component-Based Programming and then reduxed for Algorithm and Data Structures.",
                 link: "https://github.com/komoto415/TestAssignment",
             },
         ],
