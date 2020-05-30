aff7655fecd7d3f5c657a72c99723f77df48e04f
Jeffrey Ng <jng@stedwards.edu>
2020-05-30 16:50:43 -0500

    fixing file route for page note found import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProjectsComponent } from './projects/projects.component';
 import { HomeComponent } from './home/home.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PageNotFoundComponent } from './page-error/page-not-found/page-not-found.component';
 import { WegoServicesComponent } from './projects/wego-services/wego-services.component';
 import { CompToAlgoComponent } from './projects/comp-to-algo/comp-to-algo.component';
 
