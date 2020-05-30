0f3ca2f66df24119db5ccdd52a4778169875e586
Jeffrey Ng <jng@stedwards.edu>
2020-05-30 17:34:55 -0500

    adding url references for icon downloads

--- src/app/home/resume/languages/languages.component.ts
+++ src/app/home/resume/languages/languages.component.ts
@@ -7,15 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LanguagesComponent implements OnInit {
     languages: Array<string> = [
-            "python",
-            "java",
-            "sql",
-            "html",
-            "css",
-            "js",
-            // "julia",
-            // "cpp",
-            // "c",
+            "python", // https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1024px-Python-logo-notext.svg.png
+            "java", // https://images.vexels.com/media/users/3/166401/isolated/preview/b82aa7ac3f736dd78570dd3fa3fa9e24-java-programming-language-icon-by-vexels.png
+            "sql", // https://www.pngwing.com/en/free-png-zoupl
+            "html", // https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1024px-HTML5_logo_and_wordmark.svg.png
+            "css", // https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/726px-CSS3_logo_and_wordmark.svg.png
+            "js", // https://upload.wikimedia.org/wikipedia/commons/d/dc/Javascript-shield.png
+            // "julia", // https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Julia_Programming_Language_Logo.svg/1200px-Julia_Programming_Language_Logo.svg.png
+            // "cpp", // https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/911px-ISO_C%2B%2B_Logo.svg.png
+            // "c", // https://www.pinclipart.com/picdir/big/396-3965857_c-c-programming-language-logo-clipart.png
         ];
 
     constructor() { }
