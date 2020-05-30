d9bedcc665002270927263f96e4d2c5ffe4d233b
Jeffrey Ng <jng@stedwards.edu>
2020-05-30 16:52:26 -0500

    seperating project thumbnail display and project main

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -7,60 +7,23 @@ import { Router } from '@angular/router';
     styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
-    projects = [
-        [
-            {
-                title: "We Go Services",
-                thumbnail: "/wego.jpg", // https://ces.eetimes.com/wp-content/uploads/2020/01/image-eet-ces.jpg
-                tags: ["python","html5","javascript", "ngnix", "docker", "python", "swei", "agile-development", "multi-cloud-solution",],
-                desc: "In the advent of autonomous vehicles, we aim to take advantage of the growing market of Transportation as a Service with a scalable multi-service Web Application to help fulfil everyday needs.",
-                link: "https://github.com/sp20-swei-Team22",
-                router: "wego-services",
-            },
-        ],
-        [
-            {
-                title: "Components-Algorithms Bridge Assignment",
-                thumbnail: "/kart-test.jpg", // https://www.aoe.com/fileadmin/AOE.com/images/main_navigation/blog/Stock_Photos/miscellaneous/Fotolia_94900081_Chess_Pieces_930_590_70.jpg
-                tags: ["java", "components", "algorithms", "data-structures",],
-                desc: "An open bid for experimental projects that could be assigned in Component-Based Programming and then reduxed for Algorithm and Data Structures.",
-                link: "https://github.com/komoto415/TestAssignment",
-                router: "comp-to-algo",
-            },
-        ],
-        [
-            // {
-            //     title: "To Brock and not to Brock",
-            //     thumbnail: "/senior-project.png", // https://i.ibb.co/7yMB9t6/FireRed.png
-            //     tags: ["ml", "pokemon", "python", "julia"],
-            //     desc: "For my research project, my partner and I decided to teach a machine how to beat Brock in Pokemon Fire Red. If Twitch can do it, why can't a bot?",
-            // },
-        ],
-
-    ]
-
-    urlToTitle:Object = {
-        "wego-services": "We Go Services",
-        "comp-to-algo": "Components-Algorithms Bridge Assignment",
-    }
 
     constructor(public router: Router) { }
 
     ngOnInit(): void {
     }
-
-    public getProjects() {
-        return this.projects;
-    }
-
+    
     public isParentUrl() {
         return this.router.url === '/projects'
     }
 
-    public getCurretProject() {
-        var url:Array<String> = this.router.url.split('/');
-        var test:String = url[url.length - 1];
-        return this.urlToTitle[`${url[url.length - 1]}`];
+    urlToTitle: Object = {
+        "wego-services": "We Go Services",
+        "comp-to-algo": "Components-Algorithms Bridge Assignment",
     }
 
+    public getCurretProject() {
+        var url: Array<String> = this.router.url.split('/');
+        return this.urlToTitle[`${url[url.length - 1]}`];
+    }
 }
