2df1420e98d360adfc8643aaec50a3ee5a559854
Jeffrey Ng <jng@stedwards.edu>
2020-05-31 03:23:54 -0500

    moving filter component into parent for now

--- src/app/projects/project-controller/project-controller.component.ts
+++ src/app/projects/project-controller/project-controller.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-project-controller',
-  templateUrl: './project-controller.component.html',
-  styleUrls: ['../projects.component.css', './project-controller.component.css']
+    selector: 'app-project-controller',
+    templateUrl: './project-controller.component.html',
+    styleUrls: ['../projects.component.css', './project-controller.component.css']
 })
 export class ProjectControllerComponent implements OnInit {
+    constructor() { }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
