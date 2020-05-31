965405779c51e2b00ebfd02b333d4f204d99a4c4
Jeffrey Ng <jng@stedwards.edu>
2020-05-31 03:25:23 -0500

    updating url translation table

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -7,19 +7,30 @@ import { Router } from '@angular/router';
     styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
+    query: string;
 
     constructor(public router: Router) { }
 
     ngOnInit(): void {
     }
-    
+
     public isParentUrl() {
         return this.router.url === '/projects'
+    }
+
+    onKey(value: any) {
+        this.query = value.toLowerCase();
+    }
+
+    onEnter(value: string) { 
+        this.query = value.toLowerCase(); 
     }
 
     urlToTitle: Object = {
         "wego-services": "We Go Services",
         "comp-to-algo": "Components-Algorithms Bridge Assignment",
+        "first-project": "Humble Beginnings",
+        "enigma": "My Enigma Machine",
     }
 
     public getCurretProject() {
