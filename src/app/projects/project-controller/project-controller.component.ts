34368d005ff1ffce727211c04cf3007c1f341609
Jeffrey Ng <jng@stedwards.edu>
2020-06-02 12:06:18 -0500

    adding logic to see if tags are check and to clear all forms

--- src/app/projects/project-controller/project-controller.component.ts
+++ src/app/projects/project-controller/project-controller.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { LanguagesService } from 'src/app/home/resume/languages/languages.service';
+import { ProjectsService } from '../projects.service';
 
 @Component({
     selector: 'app-project-controller',
@@ -9,24 +11,93 @@ export class ProjectControllerComponent implements OnInit {
     @Output() tagEmitter = new EventEmitter<string>();
     @Output() titleEmitter = new EventEmitter<string>();
 
-    titleQuery:string;
-    tagQuery:string;
-    
-    updateTitle(value:string) {
-        this.titleQuery = value.toLowerCase();
+    @Input() filteredTagsIn: string[];
+
+    titleQuery: string;
+    tagQuery: string;
+    // tagCBDoms;
+    // lastInput:string;
+
+    constructor(public projectsService: ProjectsService, public languageService: LanguagesService) {
+        this.titleQuery = "";
+        this.tagQuery = "";
+        // this.filteredTagIn = this.projectsService.getAllTags();
+        // this.lastInput = "";
+        // this.tagCBDoms = document.getElementsByClassName("tagCB");
+    }
+
+    ngOnInit(): void {
+        this.tagQuery = this.languageService.language == null ? "" : this.languageService.language;
+    }
+
+    isChecked($event): void {
+        let label = $event.target;
+        // console.log(label.childNodes);
+        let input = label.childNodes[0];
+        let check = !input.checked;
+        let tag: string = label.childNodes[1].data.trim();
+        if (check) {
+            // console.log("I've been checked! C:")
+            // label.classList
+            this.updateTags(tag, "checked");
+        } else {
+            this.updateTags(this.tagQuery.replace(
+                this.tagQuery.split("").length == 0 ? tag.concat(" ") : tag, "").trim()
+                , "");
+        }
+        // console.log(check);
+        // console.log(tag);
+
+    }
+
+    clearForm($event): void {
+        let controllerDiv = $event.target.parentNode.parentNode.parentNode;
+        // console.log(controllerDiv);
+        let inputNodes = controllerDiv.getElementsByTagName("INPUT");
+        // console.log(inputNodes.length);
+        for (let i = 0; i < inputNodes.length; i++) {
+            let node = inputNodes[i];
+            // console.log(node);
+            switch (node.type) {
+                case "text":
+                    node.value = "";
+                    break;
+
+                case "radio":
+                    node.checked = false;
+                    break;
+
+                case "checkbox":
+                    console.log(node.checked);
+                    if (node.checked) {
+                        node.checked = false;
+                        let label = node.parentNode;
+                        console.log(label);
+                        label.classList.remove("active");
+                    }
+                    break;
+            }
+        }
+        this.updateTitle("");
+        this.updateTags("", "");
+    }
+
+    updateTitle(value: string): void {
+        this.titleQuery = value.toLowerCase().trim();
         // console.log(this.titleQuery);
         this.titleEmitter.emit(this.titleQuery);
     }
 
-    updateTags(value:string) {
-        this.tagQuery = value.toLowerCase();
+    updateTags(value: string, how: string): void {
+        let val = value.toLowerCase().trim();
+        if (how === "checked") this.tagQuery += `${this.tagQuery.length == 0 ? "" : " "}${val}`
+        else this.tagQuery = val;
         // console.log(this.tagQuery);
         this.tagEmitter.emit(this.tagQuery);
     }
 
-    constructor() { }
+    matchCheckBoxWithText(): void {
 
-    ngOnInit(): void {
     }
 
 }
