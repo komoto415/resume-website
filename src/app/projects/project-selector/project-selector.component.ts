650a5da1c070049005cfd9123bed598dc370ddb9
Jeffrey Ng <jng@stedwards.edu>
2020-06-03 02:26:46 -0500

    filter by tags now looks at all cards and will add and remove class that will hide or show

--- src/app/projects/project-selector/project-selector.component.ts
+++ src/app/projects/project-selector/project-selector.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 import { Router } from '@angular/router';
 import { Project } from '../project';
 import { ProjectsService } from '../projects.service';
@@ -8,81 +8,119 @@ import { ProjectsService } from '../projects.service';
     templateUrl: './project-selector.component.html',
     styleUrls: ['../projects.component.css', './project-selector.component.css']
 })
-export class ProjectSelectorComponent implements OnInit {
+export class ProjectSelectorComponent implements OnInit, OnChanges {
 
     @Input() titleQueryIn: string;
     @Input() tagQueryIn: string;
-    
-    @Output() tagsEmitter = new EventEmitter<string[]>();
 
-    projects: Array<Project>;
-    tags: Set<string>;
+    @Output() currentTagsEmitter = new EventEmitter<string[]>();
+
+    readonly FADE_IN_CLASS: string = "fade-in";
+    readonly FADE_OUT_CLASS: string = "fade-out";
+
+    projects: Project[];
 
     constructor(public projectsService: ProjectsService, public router: Router) {
         this.projects = this.projectsService.getProjects();
-        this.tags = new Set();
     }
 
     ngOnInit(): void {
     }
 
-    filterProjectByTitle(project: Project): boolean {
-        if (this.titleQueryIn == null) return false;
-        if (this.titleQueryIn.length === 0) return false;
-        
-        let title = project.title;
-        // console.log("#########################");
-        // console.log(this.titleQueryIn);
-
-        let trimtitle = this.titleQueryIn.trim();
-        trimtitle = trimtitle.replace(/\s+/g, " "); 
-
-        let foundTitle: boolean = title.toLowerCase().includes(trimtitle);
-        console.log("title'");
-        if (foundTitle) {
-            this.filterTags(project.tags);
-        }
-        return !foundTitle;
+    ngOnChanges(changes: SimpleChanges) {
+        // console.log(changes.tagQueryIn.previousValue);
+        console.log(changes.tagQueryIn.currentValue);
+        let oldTagQuery = changes.tagQueryIn.previousValue;
+        let newTagQuery = changes.tagQueryIn.currentValue;
+        this.filterProjects("", newTagQuery)
     }
 
-    filterProjectByTag(project: Project): boolean {
-        if (this.tagQueryIn == null) return false;
-        if (this.tagQueryIn.length === 0) return false;
-
-        let tags: string[] = project.tags;
-        // console.log("#########################");
-        // console.log(this.tagQueryIn);
-
-        let trimTag: string = this.tagQueryIn.trim();
-        trimTag = trimTag.replace(/\s+/g, " "); 
-
-        let tagList: string[] = trimTag.split(" ");
-
-        let check: number = 0;
-        for (let e in tagList) {
-            let word = tagList[e].trim();
-            if (tags.includes(word)) check += 1;
-            else check -= 1;
-        }
-
-        let foundTag: boolean = check > 0;
-        if (foundTag) {
-            this.filterTags(tags);
-        }
-        return !foundTag;
+    filterProjects(title: string, tags: string): void {
+        // this.filterProjectByTitle(title);
+        this.filterProjectByTag(tags);
     }
 
-    tagsToClass(tags: string[]): string {
-        return tags.join(' ');
+    filterProjectByTitle(title: string): void {
+        if (title == null) return;
+        // if (this.titleQueryIn == null) return false;
+        // if (this.titleQueryIn.length === 0) return false;
+
+        // // console.log("#########################");
+        // // console.log(this.titleQueryIn);
+
+        // let trimtitle = this.titleQueryIn.trim();
+        // trimtitle = trimtitle.replace(/\s+/g, " "); 
+
+        // let foundTitle: boolean = title.toLowerCase().includes(trimtitle);
+        // // this.filterTags(title, foundTitle);
     }
 
-    filterTags(tags: string[]): void {
-        // console.log("need to filter");
-        // console.log(tags);
-        // console.log(this.tags);
-        // tags.forEach(e => this.tags.add(e));
-        // console.log(this.tags);
-        // console.log([...this.tags]); 
-        // this.tagsEmitter.emit([...this.tags]);
+    filterProjectByTag(tags: string): void {
+        if (tags == null) return;
+        console.log(tags.length)
+        let tagsToKeep: string[] = [];
+        if (tags.length === 0) {
+            this.noQuery();
+            tagsToKeep = this.projectsService.getAllUniqueTags();
+        } else {
+            let trimTag = tags.trim();
+            trimTag = trimTag.replace(/\s+/g, " ");
+    
+            let tagList: string[] = trimTag.split(" ");
+    
+            let cards = document.getElementsByClassName("project-card");
+            // console.log(cards);
+            for (let div = 0; div < cards.length; div++) {
+                let card = cards[div];
+                let cardTagsAsString: string = card.getElementsByClassName("tags")[0].textContent;
+                // console.log(cardTagsAsString);
+                let removeHash = "  #";
+    
+    
+                let cardTagsAsList: string[] = cardTagsAsString.trim().split(removeHash)
+                    .map(e => e.replace(/[^a-zA-Z \-]/g, ""));
+    
+                var hasTags: boolean = tagList.every(e => {
+                    return cardTagsAsList.indexOf(e) >= 0
+                })
+    
+                let classList = card.classList;
+                let alreadyShowing = classList.contains(this.FADE_IN_CLASS);
+                if (hasTags) {
+                    if (!alreadyShowing) {
+                        classList.remove(this.FADE_OUT_CLASS);
+                        classList.add(this.FADE_IN_CLASS);
+                    }
+                    tagsToKeep = tagsToKeep.concat(cardTagsAsList);
+                } else {
+                    if (alreadyShowing) {
+                        classList.remove(this.FADE_IN_CLASS);
+                        classList.add(this.FADE_OUT_CLASS);
+                    }
+                }
+            }
+        }
+        tagsToKeep = [...new Set(tagsToKeep)];
+        this.filterTags(tagsToKeep);
+        // let titles = cards.getElementsBy
+        // this.filterTags(tags , foundTag);
+    }
+
+    noQuery(): void {
+        let cards = document.getElementsByClassName("project-card");
+        // console.log("no query");
+        for (let div = 0; div < cards.length; div++) {
+            let card = cards[div];
+            let classList = card.classList;
+            if (classList.contains(this.FADE_IN_CLASS)) continue;
+            if (classList.contains(this.FADE_OUT_CLASS)) classList.remove(this.FADE_OUT_CLASS);
+            classList.add(this.FADE_IN_CLASS);
+        }
+    }
+
+    filterTags(newTags: string[]): void {
+        // console.log("aabout to filter tags");
+        console.log(newTags);
+        setTimeout(() => this.currentTagsEmitter.emit(newTags));
     }
 }
