20e3be409593b4a347b9c0df77cf4ee73561e4dd
Jeffrey Ng <jng@stedwards.edu>
2020-06-03 02:20:51 -0500

    amde tags enum. because spelling is hard.

--- src/app/projects/project.ts
+++ src/app/projects/project.ts
@@ -1,7 +1,9 @@
+import { TagType } from './tags';
+
 export class Project {
     title: string;
     thumbnail: string;
-    tags: string[];
+    tags: TagType[];
     desc: string;
     router: string;
     alt: string;
---
+++ src/app/projects/tags.ts
@@ -0,0 +1,30 @@
+export enum TagType {
+    python = "python",
+    javascript = "javascript",
+    java ="java",
+    html = "html",
+    css = "css",
+    sql = "sql",
+    julia = "julia",
+
+    first = "first",
+    personal = "personal",
+
+    angularjs = "angularjs",
+    digitalocean = "digitalocean",
+    ngnix = "ngnix",
+    docker ="docker",
+
+    enigma = "enigma",
+    ml = "ml",
+    pokemon = "pokemon",
+
+    cs1 = "cs1",
+    components = "components",
+    algorithms = "algorithms",
+    data_structures = "data-structures",
+    swei = "swei",
+
+    web_app = "web-app",
+    cli = "cli",
+}
