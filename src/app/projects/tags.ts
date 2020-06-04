ef478043e6033c4bb202ecb93e094b9854e89398
Jeffrey Ng <komoto415@gmail.com>
2020-06-03 20:35:23 -0500

    comments just to subcatagorise enums

--- src/app/projects/tags.ts
+++ src/app/projects/tags.ts
@@ -1,4 +1,5 @@
 export enum TagType {
+    /* Languages */
     python = "python",
     javascript = "javascript",
     java ="java",
@@ -7,24 +8,30 @@ export enum TagType {
     sql = "sql",
     julia = "julia",
 
+    /*  */
     first = "first",
     personal = "personal",
 
+    /* Technologies */
     angularjs = "angularjs",
     digitalocean = "digitalocean",
     ngnix = "ngnix",
     docker ="docker",
 
+
+    /* Topics */
     enigma = "enigma",
     ml = "ml",
     pokemon = "pokemon",
 
+    /* Classes */
     cs1 = "cs1",
     components = "components",
     algorithms = "algorithms",
     data_structures = "data-structures",
     swei = "swei",
 
+    /* Form/Medium */
     web_app = "web-app",
     cli = "cli",
 }
