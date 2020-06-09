031cd64c8e6cda994727798f7e4b8e961f798348
Jeffrey Ng <jng@stedwards.edu>
2020-06-08 20:16:27 -0500

    adding interface for data on js scripts to reference

--- src/app/projects/wego-services/pageData.ts
+++ src/app/projects/wego-services/pageData.ts
@@ -1,5 +1,12 @@
-export class PageMeta {
+export interface PageMeta {
     penId: string;
     pageTitle: string;
     imgSrc: string;
+    scripts: PageScript[];
+}
+
+export interface PageScript {
+    name: string;
+    cloud: string;
+    desc: string;
 }
