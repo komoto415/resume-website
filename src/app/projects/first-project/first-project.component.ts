e9b1ce268a861fb8d65b0781efb8f4897b6381c2
Jeffrey Ng <jng@stedwards.edu>
2020-06-10 11:02:23 -0500

    moving tags to parent component

--- src/app/projects/first-project/first-project.component.ts
+++ src/app/projects/first-project/first-project.component.ts
@@ -1,21 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { TagType } from '../tags';
 
 @Component({
-  selector: 'app-first-project',
-  templateUrl: './first-project.component.html',
-  styleUrls: ['../projects.component.css', './first-project.component.css']
+    selector: 'app-first-project',
+    templateUrl: './first-project.component.html',
+    styleUrls: ['../projects.component.css', './first-project.component.css']
 })
 export class FirstProjectComponent implements OnInit {
-    tags: TagType[] = [
-        TagType.python,
-        TagType.cli,
-        TagType.first,
-        TagType.cs1,
-    ]
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+    constructor() { }
+
+    ngOnInit(): void {
+    }
 
 }
