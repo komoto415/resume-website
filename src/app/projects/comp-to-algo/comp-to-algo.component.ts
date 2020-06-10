da8723aa1dbb8288ca3e437b5cf12cbc28879f47
Jeffrey Ng <jng@stedwards.edu>
2020-06-10 11:01:54 -0500

    formatting

--- src/app/projects/comp-to-algo/comp-to-algo.component.ts
+++ src/app/projects/comp-to-algo/comp-to-algo.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-comp-to-algo',
-  templateUrl: './comp-to-algo.component.html',
-  styleUrls: ['../projects.component.css', './comp-to-algo.component.css']
+    selector: 'app-comp-to-algo',
+    templateUrl: './comp-to-algo.component.html',
+    styleUrls: ['../projects.component.css', './comp-to-algo.component.css']
 })
 export class CompToAlgoComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
