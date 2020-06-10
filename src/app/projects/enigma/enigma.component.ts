126405bc6a8daeb49c032426338a4e7254274fc7
Jeffrey Ng <jng@stedwards.edu>
2020-06-10 11:05:20 -0500

    added embedded repl of enigma machine

--- src/app/projects/enigma/enigma.component.ts
+++ src/app/projects/enigma/enigma.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-enigma',
-  templateUrl: './enigma.component.html',
-  styleUrls: ['./enigma.component.css']
+    selector: 'app-enigma',
+    templateUrl: './enigma.component.html',
+    styleUrls: ['../projects.component.css', './enigma.component.css']
 })
 export class EnigmaComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
