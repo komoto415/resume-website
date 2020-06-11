47bf4d5cf9b642fc5b0b5ecdaf62a6ce64d9f924
Jeffrey Ng <jng@stedwards.edu>
2020-06-11 11:51:21 -0500

    adding comments, visually spacing constants and variables, now checks to see if we're on the home page to start run the

--- src/app/home/countdown-to-graduation/countdown-to-graduation.component.ts
+++ src/app/home/countdown-to-graduation/countdown-to-graduation.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import * as p5 from 'p5';
 import * as moment from 'moment'; // aDD this 1 of 4
 import { BarMeta } from './BarData';
-import { ɵBrowserDomAdapter } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-countdown-to-graduation',
@@ -20,27 +19,36 @@ export class CountdownToGraduationComponent implements OnInit {
     }
 
     private createCanvas() {
-        let counterDiv = window.document.getElementById("counter");
+        let counterDiv: HTMLElement = window.document.getElementById("counter");
+        if (counterDiv == null) {
+            return;
+        }
         this.p5 = new p5(this.sketch, counterDiv);
-
         this.p5.windowResized = () => {
             counterDiv.innerHTML = "";
             this.p5 = new p5(this.sketch, counterDiv);
         }
+
     }
 
     private sketch(p: any) {
         const WRAPPER_DIV_MARGIN: number = 50;
-        let divWidth = window.document.getElementById("counter").clientWidth;
-        let canvasHeight: number = window.innerHeight / 2;
-        let maxBarWidth: number = divWidth - 2 * WRAPPER_DIV_MARGIN;
         const NUMBER_OF_TIME_BARS: number = 4;
         const FONT_Y_OFFSET: number = 14;
         const FONT_WIDTH: number = 11;
-        // const TEXT_X_OFFSET: number = 10;
         const ALICEBLUE_HEX: string = "f7f3ee";
+        const END_OF_BAR_OFFSET: number = 6; // about ish the width of 2-3 whitespaces
+
+        let counterDiv = window.document.getElementById("counter");
+        let divWidth: number = counterDiv.clientWidth;
+        let canvasHeight: number = window.innerHeight / 2;
+        let maxBarWidth: number = divWidth - 2 * WRAPPER_DIV_MARGIN;
+
+        let heightDiff: number = canvasHeight / (NUMBER_OF_TIME_BARS * 2);
+        let BAR_CONTAINER_MARGIN: number = heightDiff * .2;
+
         p.setup = (): void => {
-            p.createCanvas(divWidth, canvasHeight);
+            p.createCanvas(divWidth, canvasHeight - 2 * BAR_CONTAINER_MARGIN);
         };
 
         setInterval((): void => {
@@ -55,11 +63,8 @@ export class CountdownToGraduationComponent implements OnInit {
 
             let isOrPastGrad: boolean = ss <= 0 && mm <= 0 && hh <= 0 && DD <= 0;
             if (isOrPastGrad) {
-
+                // I'll do something special at some points
             } else {
-
-                let heightDiff: number = canvasHeight / (NUMBER_OF_TIME_BARS * 2 + 1);
-                const BAR_CONTAINER_MARGIN: number = heightDiff * .2;
                 p.strokeWeight(0);
                 p.textSize(heightDiff - 20);
                 let barDataArr: BarMeta[] = []
@@ -86,33 +91,34 @@ export class CountdownToGraduationComponent implements OnInit {
                 p.textFont("Balsamiq Sans");
 
                 barDataArr.forEach((barObj, index, _) => {
-                    const END_OF_BAR_OFFSET: number = 6; // about ish the width of 2-3 whitespaces
-                    let height: number = index * 2 * heightDiff + heightDiff
+                    let height: number = index * 2 * heightDiff + BAR_CONTAINER_MARGIN;
                     let textLegnth: number = FONT_WIDTH * barObj.desc.length;
                     let textXPosition: number = Math.max(END_OF_BAR_OFFSET * 2, barObj.width - textLegnth - END_OF_BAR_OFFSET) + WRAPPER_DIV_MARGIN;
 
+                    // the 'holsters' of the bars
                     p.fill(p.color("#1d1d33"));
                     p.rect(0, height - BAR_CONTAINER_MARGIN,
                         WRAPPER_DIV_MARGIN + barObj.widthPerUnit * 2, heightDiff + BAR_CONTAINER_MARGIN * 2,
                         0, 5, 5, 0);
 
-                    p.fill(p.color(ALICEBLUE_HEX));
-                    p.text(barObj.unit, END_OF_BAR_OFFSET * 2, height + heightDiff - FONT_Y_OFFSET);
-
+                    // 'cut out' of the bar holsters
                     p.strokeWeight(0);
                     p.fill(p.color("#2c2c4e"))
                     p.rect(WRAPPER_DIV_MARGIN, height, barObj.widthPerUnit * 2, heightDiff);
 
+                    // The actual bar of the time
                     p.fill(p.color(barObj.barColour));
                     p.rect(WRAPPER_DIV_MARGIN, height, barObj.width, heightDiff, 0, 5, 5, 0);
 
+                    // Time units display
+                    p.fill(p.color(ALICEBLUE_HEX));
+                    p.text(barObj.unit, END_OF_BAR_OFFSET * 2, height + heightDiff - FONT_Y_OFFSET);
+
+                    // Text of the numeric time
                     p.fill(p.color(ALICEBLUE_HEX));
                     p.text(barObj.desc, textXPosition, height + heightDiff - FONT_Y_OFFSET);
                 })
             }
         }, 1000);
     }
-
-
-
 }
