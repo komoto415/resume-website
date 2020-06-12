fdf8757dadcf821be090cf5ef4556b831f5f8bee
Jeffrey Ng <jng@stedwards.edu>
2020-06-12 01:10:04 -0500

    streamlining to have aboutme, education, and technologies all have the same wrapping

--- src/app/home/about-me/about-me.component.ts
+++ src/app/home/about-me/about-me.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
-  styleUrls: ['./about-me.component.css']
+  styleUrls: ['../home.component.css' ,'./about-me.component.css']
 })
 export class AboutMeComponent implements OnInit {
 
