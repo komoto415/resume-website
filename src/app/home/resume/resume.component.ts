667d9f83027ecd8afb440e84781dae7681cf5f8e
Jeffrey Ng <jng@stedwards.edu>
2020-06-12 01:10:22 -0500

    aboutme- education and technologes all have the same level or wrapping now

--- src/app/home/resume/resume.component.ts
+++ src/app/home/resume/resume.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
-  styleUrls: ['./resume.component.css']
+  styleUrls: ['../home.component.css','./resume.component.css']
 })
 export class ResumeComponent implements OnInit {
 
