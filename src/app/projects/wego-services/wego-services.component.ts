93e2ebee8716acbf97841f4ddd78968eb1a47415
Jeffrey Ng <jng@stedwards.edu>
2020-06-13 03:07:00 -0500

    added description of design process and linked google drive with all design items. Cards are now displayed with square thumbnails and are meant to be 4 per row then 2 per on small screens

--- src/app/projects/wego-services/wego-services.component.ts
+++ src/app/projects/wego-services/wego-services.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'app-wego-services',
     templateUrl: './wego-services.component.html',
-    styleUrls: ['../projects.component.css', './wego-services.component.css']
+    styleUrls: ['../../app.component.css', '../projects.component.css', './wego-services.component.css']
 })
 export class WegoServicesComponent implements OnInit {
     readonly COMMON_SERVICES: string = "common-services";
@@ -30,134 +30,122 @@ export class WegoServicesComponent implements OnInit {
         },
     ]
 
-    staticPages: PageMeta[][] = [
-        [
-            {
-                penId: "eYJNoEe",
-                pageTitle: "Landing Page",
-                imgSrc: "landing-page",
-                scripts: [
-                    {
-                        name: "landingsession.js",
-                        cloud: this.COMMON_SERVICES,
-                        desc: "",
-                    },
-                    {
-                        name: "anchorscroll.js",
-                        cloud: this.COMMON_SERVICES,
-                        desc: "",
-                    },
-                ],
-            },
-        ],
-        [
-            {
-                penId: "KKVpYeq",
-                pageTitle: "Login",
-                imgSrc: "login",
-                scripts: [
-                    {
-                        name: "login.js",
-                        cloud: this.COMMON_SERVICES,
-                        desc: "",
-                    },
-                    {
-                        name: "hideshowpassword.js",
-                        cloud: this.COMMON_SERVICES,
-                        desc: "",
-                    },
-                    {
-                        name: "datetimemethods.js",
-                        cloud: this.COMMON_SERVICES,
-                        desc: "",
-                    },
-                ],
-            },
-            {
-                penId: "JjGdVBw",
-                pageTitle: "Registration",
-                imgSrc: "registration",
-                scripts: [
-                    {
-                        name: "register.js",
-                        cloud: this.COMMON_SERVICES,
-                        desc: "",
-                    },
-                    {
-                        name: "hideshowpassword.js",
-                        cloud: this.COMMON_SERVICES,
-                        desc: "",
-                    },
-                    {
-                        name: "datetimemethods.js",
-                        cloud: this.COMMON_SERVICES,
-                        desc: "",
-                    },
-                ],
-            },
-        ],
-        [
-            {
-                penId: "ExPjJee",
-                pageTitle: "Customer Dashboard ",
-                imgSrc: "customer-dashboard",
-                scripts: [],
-            },
-
-            {
-                penId: "",
-                pageTitle: "Fleet Manager Dashboard",
-                imgSrc: "fleet-manager-dashboard",
-                scripts: [],
-            },
-        ],
-        [
-            {
-                penId: "KKVpYrX",
-                pageTitle: "Dry Cleaning Order Page",
-                imgSrc: "dry-cleaning-order",
-                scripts: [
-                    {
-                        name: "onload.js",
-                        cloud: this.DEMAND_FRONT_END,
-                        desc: "",
-                    }
-                ],
-            },
-            {
-                penId: "xxZGemm",
-                pageTitle: "Order Confirmation Page",
-                imgSrc: "order-confirmation",
-                scripts: [
-                    {
-                        name: "getcourierinfo.js",
-                        cloud: this.DEMAND_FRONT_END,
-                        desc: "",
-                    }
-                ],
-            },
-        ],
-        [
-            {
-                penId: "vYLLjYj",
-                pageTitle: "Signout Confirmation",
-                imgSrc: "signout-confirmation",
-                scripts: [],
-            },
-        ],
+    staticPages: PageMeta[] = [
+        {
+            penId: "eYJNoEe",
+            pageTitle: "Landing Page",
+            imgSrc: "landing-page",
+            scripts: [
+                {
+                    name: "landingsession.js",
+                    cloud: this.COMMON_SERVICES,
+                    desc: "",
+                },
+                {
+                    name: "anchorscroll.js",
+                    cloud: this.COMMON_SERVICES,
+                    desc: "",
+                },
+            ],
+        },
+        {
+            penId: "KKVpYeq",
+            pageTitle: "Login",
+            imgSrc: "login",
+            scripts: [
+                {
+                    name: "login.js",
+                    cloud: this.COMMON_SERVICES,
+                    desc: "",
+                },
+                {
+                    name: "hideshowpassword.js",
+                    cloud: this.COMMON_SERVICES,
+                    desc: "",
+                },
+                {
+                    name: "datetimemethods.js",
+                    cloud: this.COMMON_SERVICES,
+                    desc: "",
+                },
+            ],
+        },
+        {
+            penId: "JjGdVBw",
+            pageTitle: "Registration",
+            imgSrc: "registration",
+            scripts: [
+                {
+                    name: "register.js",
+                    cloud: this.COMMON_SERVICES,
+                    desc: "",
+                },
+                {
+                    name: "hideshowpassword.js",
+                    cloud: this.COMMON_SERVICES,
+                    desc: "",
+                },
+                {
+                    name: "datetimemethods.js",
+                    cloud: this.COMMON_SERVICES,
+                    desc: "",
+                },
+            ],
+        },
+        {
+            penId: "vYLLjYj",
+            pageTitle: "Signout Confirmation",
+            imgSrc: "signout-confirmation",
+            scripts: [],
+        },
+        {
+            penId: "ExPjJee",
+            pageTitle: "Customer Dashboard ",
+            imgSrc: "customer-dashboard",
+            scripts: [],
+        },
+        {
+            penId: "KKVpYrX",
+            pageTitle: "Dry Cleaning Order Page",
+            imgSrc: "dry-cleaning-order",
+            scripts: [
+                {
+                    name: "onload.js",
+                    cloud: this.DEMAND_FRONT_END,
+                    desc: "",
+                }
+            ],
+        },
+        {
+            penId: "xxZGemm",
+            pageTitle: "Order Confirmation Page",
+            imgSrc: "order-confirmation",
+            scripts: [
+                {
+                    name: "getcourierinfo.js",
+                    cloud: this.DEMAND_FRONT_END,
+                    desc: "",
+                }
+            ],
+        },
+        {
+            penId: "",
+            pageTitle: "Fleet Manager Dashboard",
+            imgSrc: "fleet-manager-dashboard",
+            scripts: [],
+        },
     ];
-    
+
     constructor() { }
 
     ngOnInit(): void {
     }
 
-    getCodePenURL(id:string):string {
+    getCodePenURL(id: string): string {
         return `https://codepen.io/komoto415/full/${id}`;
     }
 
     getIdFromPageTitle(pageTitle: string): string {
         return pageTitle.toLowerCase().trim().replace(/\ /g, '-');
     }
-
 }
